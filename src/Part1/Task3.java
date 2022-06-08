250164dd07782925fc53b8de4b6fd0500199e46e
DimitriusSid <dmitrij.schidr@gmail.com>
2022-06-08 12:16:24 +0300

    Solution for task 1.3

---
+++ src/Part1/Task3.java
@@ -0,0 +1,37 @@
+/**
+ * Инвертирование букв и слов.
+ * Написать программу, которая инвертирует буквы каждого слова, и программу,
+ * которая инвертирует буквы каждого слова и сами слова
+ */
+
+
+package Part1;
+
+import java.util.*;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println(inversionV1("Victoria"));
+        System.out.println(inversionV2("Victoria"));
+    }
+
+    public static String inversionV2(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.reverse().toString();
+
+    }
+
+    public static String inversionV1(String string) {
+        String[] elements = string.split("");
+        Deque<String> deque = new ArrayDeque<>();
+        for (String el : elements) {
+            deque.addFirst(el);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String str : deque) {
+            stringBuilder.append(str);
+        }
+        return stringBuilder.toString();
+    }
+
+}
