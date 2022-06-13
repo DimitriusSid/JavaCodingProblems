b6222e29ccebbe74fbcbf01736ea3225d434b726
DimitriusSid <dmitrij.schidr@gmail.com>
2022-06-13 11:36:57 +0300

    Solution for task 1.7

---
+++ src/Part1/Task7.java
@@ -0,0 +1,29 @@
+/**
+ * Конвертирование строки в значение типа int, long, float или double.
+ * Написать программу, которая конвертирует заданный объект типа string (представляющий число) в значение типа int, long, float или double
+ */
+
+package Part1;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int convertToInt(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static long convertToLong(String s) {
+        return Long.parseLong(s);
+    }
+
+    public static float convertToFloat(String s) {
+        return Float.parseFloat(s);
+    }
+
+    public static double convertToDouble(String s) {
+        return Double.parseDouble(s);
+    }
+
+}
