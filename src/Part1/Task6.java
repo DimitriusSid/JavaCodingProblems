c7a394efdfada46070b4db6eae232ce79e5db019
DimitriusSid <dmitrij.schidr@gmail.com>
2022-06-13 11:26:31 +0300

    Solution for task 1.6

---
+++ src/Part1/Task6.java
@@ -0,0 +1,32 @@
+/**
+ * Подсчет появлений некоторого символа.
+ * Написать программу, которая подсчитывает появления того или иного символа в заданной строке.
+ */
+
+
+package Part1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println(countOfTheCharacter("qwertyzz"));
+
+    }
+
+    public static Map<Character, Integer> countOfTheCharacter(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        int count = 1;
+        for (char ch : chars) {
+            if (map.containsKey(ch)) {
+                map.replace(ch, count + 1);
+            } else {
+                map.put(ch, count);
+            }
+        }
+        return map;
+    }
+
+}
